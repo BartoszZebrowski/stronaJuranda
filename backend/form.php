b8bacc3f7f0b2b5967afd402245af1514f6a72fe
Gibon15 <b.zebrowski16@wp.pl>
2021-04-07 22:16:59 +0200

    W trakcie tworzena backendu 2 - wrzucanie na serwer googla 2

--- backend/form.php
+++ backend/form.php
@@ -11,11 +11,11 @@ $head =
     "Content-Transfer-Encoding: 8bit";
 
 $od = "\nFrom: test@jurand.cba.pl";
-$email = 'test@jurand.cba.pl';
+$email = 'b.zebrowski16@wp.pl';
 $subject = 'temat';
 $message = 'Dziękujemy za wysłanie formularza';
 $error = 'Wystąpił błąd podczas wysyłania formularza';
 $charset = 'iso-8859-2';
 $body = '';
 
-echo mail($email, $subject, $body, $head, $od) ? $message : $error;
+echo mail($email, $subject, $tekst) ? $message : $error;
